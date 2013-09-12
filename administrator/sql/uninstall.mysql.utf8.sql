8d10603fc75ccfa867e51d2d98f242af5392c57d
Lê Hoàng Phương <herophuong93@gmail.com>
2013-09-12 09:16:31 +0700

    add query to remove content type upon uninstalling

--- administrator/sql/uninstall.mysql.utf8.sql
+++ administrator/sql/uninstall.mysql.utf8.sql
@@ -3,3 +3,5 @@ DROP TABLE IF EXISTS `#__dzproduct_fields`;
 DROP TABLE IF EXISTS `#__dzproduct_groups`;
 DROP TABLE IF EXISTS `#__dzproduct_groupcat_relations`;
 DROP TABLE IF EXISTS `#__dzproduct_field_data`;
+
+DELETE FROM `#__content_types` WHERE `type_alias` = 'com_dzproduct.item';
