288a02238b58e098ab447507d1f8c37593a9bf6d
Lê Hoàng Phương <herophuong93@gmail.com>
2013-10-08 12:19:59 +0700

    prepare for v1.1.0

--- script.php
+++ script.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @version     1.0.0
- * @package     com_dztour
+ * @version     1.1.0
+ * @package     com_dzproduct
  * @copyright   Copyright (C) 2013. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  * @author      DZ Team <dev@dezign.vn> - dezign.vn
